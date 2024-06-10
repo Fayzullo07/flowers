bd5ef1b28c2ae49e2699e00cd71681231ac9f733
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-06-10 22:40:54 +0500

    images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,15 +8,15 @@ const Hero = () => {
 
                 <div className="grid grid-cols-1 content-center gap-4">
                     <Link to="/typeOfMaking" className="text-center">
-                        <img width={160} className="hover:scale-105 duration-300  mx-auto" src="src/assets/images/flowers1.png" alt="flowers" />
+                        <img width={160} className="hover:scale-105 duration-300  mx-auto" src="public/flowers1.png" alt="flowers" />
                         <button className=" hover:px-8 duration-300 border-2 border-red-600 bg-red-400 py-1 px-6 rounded-full text-white text-2xl">Bouquet</button>
                     </Link>
                     <Link to="/typeOfMaking" className="text-center">
-                        <img width={160} className=" hover:scale-105 duration-300 mx-auto" src="src/assets/images/flowers2.png" alt="flowers" />
+                        <img width={160} className=" hover:scale-105 duration-300 mx-auto" src="public/flowers2.png" alt="flowers" />
                         <button className="hover:px-8 duration-300 border-2 border-red-600 bg-red-400 py-1 px-6 rounded-full text-white text-2xl">Box</button>
                     </Link>
                     <Link to="/typeOfMaking" className="text-center">
-                        <img width={160} className="hover:scale-105 duration-300  mx-auto" src="src/assets/images/flowers3.png" alt="flowers" />
+                        <img width={160} className="hover:scale-105 duration-300  mx-auto" src="public/flowers3.png" alt="flowers" />
                         <button className="hover:px-8 duration-300 border-2 border-red-600 bg-red-400 py-1 px-6 rounded-full text-white text-2xl">Vase</button>
                     </Link>
                 </div>
